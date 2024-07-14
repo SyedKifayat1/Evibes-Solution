e1964463fb49248bc3bc00ced194eb99280b27b7
Syed Kifayat <bsce22026@itu.edu.pk>
2024-07-15 00:38:01 +0500

    commit

--- src/Components/services/seo/index.jsx
+++ src/Components/services/seo/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import SEOBoost from './seo-boost'
 import SEOProcess from './seo-process'
+import SEONeed from './seo-need'
 
 const SEOService = () => {
   return (
     <div>
         <SEOBoost/>
         <SEOProcess/>
+        <SEONeed/>
     </div>
   )
 }

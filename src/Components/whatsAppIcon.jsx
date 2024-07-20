36e1172f8005e7ed3e219140878f46af42911c86
Syed Kifayat <bsce22026@itu.edu.pk>
2024-07-20 23:02:31 +0500

    commit

--- src/Components/whatsAppIcon.jsx
+++ src/Components/whatsAppIcon.jsx
@@ -3,7 +3,7 @@ import './whats-app-icon.css'
 const WhatsAppIcon = () => {
   return (
     <div className='whatsapp-btn-container'>
-        <a className='whatsapp-btn' target='_blank' href="http://wa.me/923118120088"><i class="fa-brands fa-whatsapp"></i></a>
+        <a className='whatsapp-btn' target='_blank' href="http://wa.me/923269411582"><i class="fa-brands fa-whatsapp"></i></a>
     </div>
   )
 }
